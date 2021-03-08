5d7c539cde9ea779ec5dbb5dd4abd7c6587af5d9
titusadt <61164375+titusadt@users.noreply.github.com>
2021-03-07 21:28:57 -0800

    fixed the code to pass the tests

--- leap_year.py
+++ leap_year.py
@@ -2,10 +2,10 @@
 
 def check_year(prompt):
    if prompt % 4 == 0 and prompt % 100 != 0:
-      print(prompt, "is a leap year")
+      return "Leap Year"
    elif prompt % 400 == 0:
-      print(prompt, "is a Leap Year")
+      return "Leap Year"
    elif prompt % 100 == 0:
-      print(prompt, "is not a Leap Year")
+      return "not Leap Year"
    else:
-      print(prompt, "is not a Leap Year")
+      return "not Leap Year"
